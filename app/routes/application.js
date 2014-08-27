0486121655f9decefdfb74d18ac0a5e6ba234464
Jamie White <jamie@jgwhite.co.uk>
2014-08-27 10:14:46 +0100

    Correct reference to Ember in Application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
     return this.get('session').fetch()
-      .catch(Em.run.bind(this, function() {
+      .catch(Ember.run.bind(this, function() {
         this.transitionTo('sign-in');
       }));
   },
